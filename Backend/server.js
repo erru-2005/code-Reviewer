805b7d86fddd85131366e32b99eeba7c0bd3a78f
errol <errualmeida2005@gmail.com>
2025-02-22 21:33:30 +0530

    correcting port

--- Backend/server.js
+++ Backend/server.js
@@ -1,4 +1,5 @@
 const app = require('./src/app');
+const PORT = process.env.PORT || 3000;
 
 
 
@@ -6,7 +7,6 @@ const app = require('./src/app');
 
 
 
-
-app.listen(3000, () => {
-    console.log("server started on port http://localhost:3000");
+app.listen(PORT, () => {
+    console.log(`server started on port http://localhost:${PORT}`);
 });
